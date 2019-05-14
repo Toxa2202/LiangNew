91dcf220bf161d8593d1a98a36f2e64d5144630a
Anton <svyatoj@gmail.com>
2019-05-14 22:56:42 +0300

    Add to Git Repository

---
+++ chapter_12_GUI_Basics/src/Lab_126_DisplayIcons/Lab_126_DisplayIcons.java
@@ -0,0 +1,37 @@
+package Lab_126_DisplayIcons;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import javax.swing.ImageIcon;
+
+public class Lab_126_DisplayIcons extends JFrame {
+
+    public Lab_126_DisplayIcons() {
+        setLayout(new GridLayout(2, 2, 0, 0));
+        // Did not see images. Must to provide whole path to those images
+        JLabel jlbl1 = new JLabel(new ImageIcon("D:/JAVA/LIANG/chapter_12_GUI_Basics/src/images/england.jpg"));
+        JLabel jlbl2 = new JLabel(new ImageIcon("D:/JAVA/LIANG/chapter_12_GUI_Basics/src/images/france.jpg"));
+        JLabel jlbl3 = new JLabel(new ImageIcon("D:/JAVA/LIANG/chapter_12_GUI_Basics/src/images/norway.jpg"));
+        JLabel jlbl4 = new JLabel(new ImageIcon("D:/JAVA/LIANG/chapter_12_GUI_Basics/src/images/puertorico.jpg"));
+
+        jlbl1.setBorder(new LineBorder(Color.BLACK));
+        jlbl2.setBorder(new LineBorder(Color.BLACK));
+        jlbl3.setBorder(new LineBorder(Color.BLACK));
+        jlbl4.setBorder(new LineBorder(Color.BLACK));
+
+        add(jlbl1);
+        add(jlbl2);
+        add(jlbl3);
+        add(jlbl4);
+    }
+
+    public static void main(String[] args) {
+        Lab_126_DisplayIcons frame = new Lab_126_DisplayIcons();
+        frame.setTitle("Excercise 12_6");
+        frame.setSize(200, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
