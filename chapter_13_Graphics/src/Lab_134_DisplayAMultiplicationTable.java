f94f45bf590aa95aab858ebb2b188175614cece2
anton.sviatov <svyatoj@gmail.com>
2019-05-27 21:32:17 +0300

    Add to Git Repository. Exercise 4. Multiplication table.

---
+++ chapter_13_Graphics/src/Lab_134_DisplayAMultiplicationTable.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by anton.sviatov on 27.05.2019.
+ */
+public class Lab_134_DisplayAMultiplicationTable extends JFrame {
+    public Lab_134_DisplayAMultiplicationTable() {
+        add(new MultiplPanel());
+    }
+    public static void main(String[] args) {
+        Lab_134_DisplayAMultiplicationTable frame = new Lab_134_DisplayAMultiplicationTable();
+        frame.setTitle("Exercise13_4");
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
+class MultiplPanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(new Font("SansSerif", Font.BOLD, 20));
+        g.drawString("Multiplication Table", 65, 20);
+        g.setFont(new Font("SansSerif", Font.ITALIC, 14));
+
+        for (int i = 0; i < 9; i++) {
+            g.drawString((i + 1) + "", 70 + i * 30, 50);
+            g.drawString((i + 1) + "", 30, 80 + i * 30);
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                g.drawString(((i + 1) * (j + 1)) + "", 70 + i * 30, 80 + j * 30);
+            }
+        }
+        g.drawRect(60, 60, 270, 270);
+    }
+}
