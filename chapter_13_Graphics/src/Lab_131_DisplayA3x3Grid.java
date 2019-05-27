f4dbc8696c73ee1c3c8b0fd10d68ccfd26a0b83b
anton.sviatov <svyatoj@gmail.com>
2019-05-27 18:45:15 +0300

    Add to Git Repository. Excercise 1. Draw several lines with different color.

---
+++ chapter_13_Graphics/src/Lab_131_DisplayA3x3Grid.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by anton.sviatov on 27.05.2019.
+ */
+public class Lab_131_DisplayA3x3Grid extends JFrame {
+    public Lab_131_DisplayA3x3Grid() {
+        add(new NewPanel());
+    }
+
+    public static void main(String[] args) {
+        Lab_131_DisplayA3x3Grid frame = new Lab_131_DisplayA3x3Grid();
+        frame.setTitle("Excercise13_1");
+        frame.setSize(300, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    class NewPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            g.setColor(Color.BLUE);
+            g.drawLine(0, 50, 300, 50);
+            g.drawLine(0, 100, 300, 100);
+            g.setColor(Color.RED);
+            g.drawLine(100, 0, 100, 200);
+            g.drawLine(200, 0, 200, 200);
+        }
+    }
+}
