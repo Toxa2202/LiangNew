f22c7dbb1ef8f202e0a5d4064eac6295a12bcb20
anton.sviatov <svyatoj@gmail.com>
2019-05-28 20:10:12 +0300

    Add to Git Repository. Exercise 10. Draw cylinder from two lines, one oval and six arcs downside.

---
+++ chapter_13_Graphics/src/Lab_1310_DisplayACylinder.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by anton.sviatov on 28.05.2019.
+ */
+public class Lab_1310_DisplayACylinder extends JFrame {
+    public Lab_1310_DisplayACylinder() {
+        add(new CylinderLabel());
+    }
+
+    public static void main(String[] args) {
+        Lab_1310_DisplayACylinder frame = new Lab_1310_DisplayACylinder();
+        frame.setTitle("Exercise13_10");
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
+
+class CylinderLabel extends JLabel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        g.drawOval((int)(0.1 * width), (int)(0.1 * height), (int)(0.8 * width), (int)(0.25 * height));
+        g.drawLine((int)(0.1 * width), (int)(0.22 * height), (int)(0.1 * width), (int)(0.78 * height));
+        g.drawLine((int)(0.9 * width), (int)(0.22 * height), (int)(0.9 * width), (int)(0.78 * height));
+        g.drawArc((int)(0.1 * width), (int)(0.65 * height), (int)(0.8 * width), (int)(0.25 * height), 180, 180);
+        g.drawArc((int)(0.1 * width), (int)(0.65 * height), (int)(0.8 * width), (int)(0.25 * height), 0, 20);
+        g.drawArc((int)(0.1 * width), (int)(0.65 * height), (int)(0.8 * width), (int)(0.25 * height), 40, 20);
+        g.drawArc((int)(0.1 * width), (int)(0.65 * height), (int)(0.8 * width), (int)(0.25 * height), 80, 20);
+        g.drawArc((int)(0.1 * width), (int)(0.65 * height), (int)(0.8 * width), (int)(0.25 * height), 120, 20);
+        g.drawArc((int)(0.1 * width), (int)(0.65 * height), (int)(0.8 * width), (int)(0.25 * height), 160, 20);
+    }
+}
