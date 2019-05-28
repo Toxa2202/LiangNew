9c8d9e002a135a3602603cf285a632eef9cef8e1
anton.sviatov <svyatoj@gmail.com>
2019-05-28 15:12:02 +0300

    Add to Git Repository. Exercise 7. Tic Tac Toe game.

---
+++ chapter_13_Graphics/src/Lab_137_TicTacToeBoard.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by anton.sviatov on 28.05.2019.
+ */
+public class Lab_137_TicTacToeBoard extends JFrame {
+    public Lab_137_TicTacToeBoard() {
+        setLayout(new GridLayout(3, 3, 5, 5));
+
+        int size = 9;
+        for (int i = 0; i < size; i++) {
+            add(new CustomPanel());
+        }
+    }
+
+    public static void main(String[] args) {
+        Lab_137_TicTacToeBoard frame = new Lab_137_TicTacToeBoard();
+        frame.setTitle("Exercise13_7");
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
+class CustomPanel extends JPanel{
+    public static final int LINES = 1;
+    public static final int OVAL = 2;
+    public static final int EMPTY = 3;
+    private int type = (int) (Math.floor(Math.random() * 3) + 1);
+
+    public CustomPanel() {
+
+    }
+
+    public CustomPanel(int type) {
+        this.type = type;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        switch (type) {
+            case LINES:
+                g.drawLine(10, 10, width - 10, height - 10);
+                g.drawLine(width - 10, 10, 10, height - 10);
+                break;
+            case OVAL:
+                g.drawOval((int)(0.1 * width), (int)(0.1 * height),
+                        (int)(0.8 * width), (int)(0.8 * height));
+            case EMPTY:
+                // do nothing
+        }
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+        repaint();
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(80, 80);
+    }
+}
