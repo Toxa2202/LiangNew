0af56a3a62ee93a3940a2b1d6b7b535e6a8285db
anton.sviatov <svyatoj@gmail.com>
2019-06-18 12:12:13 +0300

    Add to Git. Exercise 06. Hex to dec with NumberFormatException.

---
+++ chapter_14_ExceptionsHandlingAndTextIO/src/Exercise_06.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Created by anton.sviatov on 18.06.2019.
+ */
+public class Exercise_06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a hex number: ");
+        String hex = input.nextLine();
+
+        System.out.println("The decimal value for the hex number " + hex +
+                " is " + hexToDecimal(hex.toUpperCase()));
+    }
+
+    public static int hexToDecimal(String hex) {
+        int decimalValue = 0;
+        for (int i = 0; i < hex.length(); i++) {
+            char hexChar = hex.charAt(i);
+            decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
+        }
+        return decimalValue;
+    }
+
+    public static int hexCharToDecimal(char ch) {
+        if (ch >= 'A' && ch <= 'F') {
+            return 10 + ch - 'A';
+        } else if (ch >= '0' && ch <= '7') {
+            return ch - '0';
+        } else {
+            throw new NumberFormatException();
+        }
+    }
+}
